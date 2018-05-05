8f3711086ac096a3877251a30f064cb53bbf120e
Emerson Doyah <33679458+emerzonic@users.noreply.github.com>
2018-05-05 11:59:15 -0500

    Update elements.js

--- assets/javascript/elements.js
+++ assets/javascript/elements.js
@@ -1,53 +1,53 @@
 var $resultPage = `<div class="row">
-                            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                                <div class="imageDiv">
-                                    <img class="img-fluid max-width: 100% index" id="resultImage" alt="">
-                                </div>
-                            </div>
-                            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                                <div class="row">
-                                    <div class="col-12"><div class="title">
-                                        <h1 class="index" id="titleName"></h1>
-                                    </div>
-                                </div>
-                                    <div class="col-12"><div class="category">
-                                       <h2 id="category"></h2>
-                                    </div>
-                                </div>
-                                    <div class="col-12"><div class="region">
-                                        <h1 id="region"></h1>
-                                    </div>
-                                </div>
-                                    
-                                </div>
-                                </div>
-                            </div>
-                        </div><hr>`;
+                      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                          <div class="imageDiv">
+                              <img class="img-fluid max-width: 100% index" id="resultImage" alt="">
+                          </div>
+                      </div>
+                      <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                          <div class="row">
+                              <div class="col-12"><div class="title">
+                                  <h1 class="index" id="titleName"></h1>
+                              </div>
+                          </div>
+                              <div class="col-12"><div class="category">
+                                 <h2 id="category"></h2>
+                              </div>
+                          </div>
+                              <div class="col-12"><div class="region">
+                                  <h1 id="region"></h1>
+                              </div>
+                          </div>     
+                       </div>
+                    </div>
+                   </div>
+               </div>
+             <hr>`;
 
 var $page = `<div class="container" id="container"></div>
-<div id='modal' class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-lg">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel"></h5>
-            <button id="closeModal" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-        <div class="modal-body"  id="modalBody">
-        </div>
-    </div>
-</div>
-</div>   
-<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-sm">
-    <div class="modal-content">
-    <button type="button" class="btn btn-success" data-dismiss="modal">Meal Has Been Added!</button>   
-        <div id=""saveMessage>
-            <img class="save" src="assets/images/save.gif">
-        </div>
-    </div>
-</div>
-</div>
-</div>`;
+                 <div id='modal' class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                     <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                           <div class="modal-header">
+                               <h5 class="modal-title" id="exampleModalLabel"></h5>
+                               <button id="closeModal" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <div>
+                            <div class="modal-body"  id="modalBody">
+                         </div>
+                     </div>
+                 </div>
+                 </div>   
+                 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                   <div class="modal-dialog modal-sm">
+                       <div class="modal-content">
+                           <button type="button" class="btn btn-success" data-dismiss="modal">Meal Has Been Added!</button>   
+                               <div id=""saveMessage>
+                                   <img class="save" src="assets/images/save.gif">
+                               </div>
+                           </div>
+                         </div>
+                      </div>
+                 </div>`;
 
 var $headerDiv = `<div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xm-12">  
@@ -68,9 +68,9 @@ var $headerDiv = `<div class="row">
                       <div class="row">
                           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <div class="buttonDiv">
-                                        <button class="btn btn-danger btn-lg justify-content-center" id="video" data-toggle="modal" data-target=".bd-example-modal-lg">Watch Video</button>
-                                        <a class="btn btn-primary btn-lg justify-content-center" target="_blank" id="sourceLink">Learn More</a>
-                                        <button class="btn btn-secondary btn-lg justify-content-center" id="firebase" data-toggle="modal" data-target=".bd-example-modal-sm">Add to Favorites</button>
+                                    <button class="btn btn-danger btn-lg justify-content-center" id="video" data-toggle="modal" data-target=".bd-example-modal-lg">Watch Video</button>
+                                    <a class="btn btn-primary btn-lg justify-content-center" target="_blank" id="sourceLink">Learn More</a>
+                                    <button class="btn btn-secondary btn-lg justify-content-center" id="firebase" data-toggle="modal" data-target=".bd-example-modal-sm">Add to Favorites</button>
                                 </div>
                           </div>
                       </div>
@@ -94,7 +94,6 @@ var $headerDiv = `<div class="row">
                                         </tr>
                                     </thead>
                                     <tbody id="ingredientsTable">
-                                    
                                     </tbody>
                                 </table>
                             </div>
@@ -105,15 +104,14 @@ var $headerDiv = `<div class="row">
 
 
 var $instDiv = `<div class="row">
-
-<div class="col-lg-12">
-    <div id="instDiv">
-    <hr>
-    <h3 class="headings">INSTRUCTIONS</h3>
-    <p class="instructions"></p>  
-    </div>
-</div>
-</div>`;
+                  <div class="col-lg-12">
+                      <div id="instDiv">
+                      <hr>
+                      <h3 class="headings">INSTRUCTIONS</h3>
+                      <p class="instructions"></p>  
+                      </div>
+                  </div>
+                </div>`;
   
 
 var $homePageContent = `<div class="row justify-content-lg-center">
