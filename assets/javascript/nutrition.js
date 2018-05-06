e6fb6720c4049a63fc17c1169352b3bdd3ba1462
Emerson Doyah <33679458+emerzonic@users.noreply.github.com>
2018-05-06 17:12:45 -0500

    Update nutrition.js

--- assets/javascript/nutrition.js
+++ assets/javascript/nutrition.js
@@ -20,7 +20,6 @@ $(document).on('click', '.mealItem', function () {
   // this query uses the search term from the first query to find the nutrient profile of the food
   $.ajax(common).done(function (response) {
     var nutrientItem = response.common[0].food_name;
-    // console.log(nutrientItem);
     var settings = {
       "url": "https://trackapi.nutritionix.com/v2/natural/nutrients",
       "method": "POST",
@@ -36,24 +35,16 @@ $(document).on('click', '.mealItem', function () {
 
     // this area enters the name of the food, the serving size, list of nutrients and nutrient amount for the selected ingredient into a table
     $.ajax(settings).done(function (response) {
-      // store the food result to a food variable
       var food = response.foods[0];
-      console.log(food);
-      //get the serving weight per gram
       var gramEquation = 100 / (food.serving_weight_grams);
-      //variable for servicing size
       var servingSize = ("Per "+food.serving_unit+" ("+food.serving_weight_grams+" grams)");
       // create two empty arrays to store object keys as string and their values
       var nutrientNames = [];
       var nutrientValues = [];
-      // create a variable to hold the object properties
       var keys = Object.keys(food);
-      //loop through the properties
       keys.forEach(function (key) {
-        // if property starts with 'nf_' and not blank/empty
+        // if property starts with 'nf_' and not blank/empty push it into the arrays
         if (/nf_.*/.test(key) && food[key] !== null && food[key] !== '') {
-          //remove the nf_ from the nutrient name and make the first letter uppercase
-          //push both the nutrient name and value into their arrays
           nutrientNames.push(key.charAt(3).toUpperCase() + key.substr(4));
           nutrientValues.push(food[key]);
         }
@@ -61,20 +52,14 @@ $(document).on('click', '.mealItem', function () {
       
       //append the table that will hold the nutrient info onto the modal body
       $('#modalBody').append($nutrientTable);
-      //loop their both nutrientNames and nutrientValues array
       for (var l = 0; l < nutrientNames.length && l < nutrientValues.length; l++) {
-        //assign each nutrient Name and nutrient value to a variable
         var nutrName = nutrientNames[l];
         var nutrValue = nutrientValues[l];
-        //create a jquery variable to hold each nutrient row 
-        //replace the underscores with space
-        //round up the values
         var $nutrRow = `<tr class="nutrientRow">
                            <td>${nutrName.split('_').join(' ')}</td>
                            <td>${Math.round(nutrValue)}</td>
                            <td>${Math.round(gramEquation * nutrValue)}</td>
                        </tr>`;
-         //append the nutrient row to the nutriention table on the modal
          $('#nutritionTable').append($nutrRow);
         }
      
